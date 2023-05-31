a276c9ec264439f2762e310988366de7925af862
Sharith Blanco <117848509+Sblanco16@users.noreply.github.com>
2023-05-31 12:23:27 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Formulario from './componentes/formulario'
+import Formulario from '/componentes/formulario'
 
 function App() {
   const [count, setCount] = useState(0)
